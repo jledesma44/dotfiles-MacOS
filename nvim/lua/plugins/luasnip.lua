8bc11187f0ce659a4ddd81f6aa21a718d9b705be
jledesma44 <49803962+jledesma44@users.noreply.github.com>
2026-02-18 13:30:31 -0800

    nvim was added as the default neovim folder

---
+++ nvim/lua/plugins/luasnip.lua
@@ -0,0 +1,14 @@
+return {
+  "L3MON4D3/LuaSnip",
+  version = "v2*",
+  dependencies = {
+    {
+      "rafamadriz/friendly-snippets",
+      opts = {},
+      config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
+        require("luasnip.loaders.from_lua").load()
+      end,
+    },
+  },
+}
