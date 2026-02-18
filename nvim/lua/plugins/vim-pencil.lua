4826a539e0a9b79b6079d23cd494c05d9df91813
jledesma44 <49803962+jledesma44@users.noreply.github.com>
2026-02-18 13:51:53 -0800

    removed the config function from the vim-pencil plugin spec

--- nvim/lua/plugins/vim-pencil.lua
+++ nvim/lua/plugins/vim-pencil.lua
@@ -1,13 +1,3 @@
 return {
   { "preservim/vim-pencil" },
-  ft = { "html" }, -- This ensures the plugin is loaded for html files
-  config = function()
-    -- Autocommand to run PencilSoft specifically for html file types
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = "html",
-      callback = function()
-        vim.cmd("PencilSoft")
-      end,
-    })
-  end,
 }
