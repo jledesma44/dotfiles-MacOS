4e6857d6154cbe7eb7949c876c3fff07627046fc
jledesma44 <49803962+jledesma44@users.noreply.github.com>
2026-02-18 13:29:51 -0800

    Lazyvim folder deleted from dotfiles

--- Lazyvim/lua/after/ftplugin/yaml.lua
+++
@@ -1,5 +0,0 @@
--- ~/.config/nvim/after/ftplugin/python.lua
-vim.opt_local.tabstop = 2 -- A TAB character looks like 4 spaces
-vim.opt_local.shiftwidth = 2 -- Number of spaces inserted when indenting
-vim.opt_local.softtabstop = 2 -- Number of spaces inserted instead of a TAB character
-vim.opt_local.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
--- Lazyvim/lua/plugins/px-to-rm.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "jsongerber/nvim-px-to-rem",
-    -- If you need to set some options replace the line above with:
-    config = function()
-      require("nvim-px-to-rem").setup()
-    end,
-  },
-}
