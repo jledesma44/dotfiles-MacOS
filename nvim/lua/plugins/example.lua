ab843f3090af125ab0972847bc9781763ebb249c
jledesma44 <49803962+jledesma44@users.noreply.github.com>
2026-02-18 22:23:28 -0800

    removed plugin williamboman/mason.nvim from example.lua  and created a mason.lua config file

--- nvim/lua/plugins/example.lua
+++ nvim/lua/plugins/example.lua
@@ -187,9 +187,26 @@ return {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
+        -- LSPs
+        "lua-language-server",
+        "emmet-language-server",
+        "typescript-language-server",
+        "eslint-lsp",
+        "json-lsp",
+        "marksman",
         "stylua",
+        "tailwindcss-languge-server",
+
+        -- DAPs
+        "js-debug-adapter",
+
+        -- Formatters
+        "prettier",
         "shellcheck",
         "shfmt",
+
+        -- Linters
+        "eslint_d",
         "flake8",
       },
     },
