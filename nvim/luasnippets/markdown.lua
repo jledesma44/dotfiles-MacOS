5266a60d5360cb74f0a1f658f03b969f4e52c5c4
jledesma44 <49803962+jledesma44@users.noreply.github.com>
2026-04-04 10:44:20 -0700

    added a markdown.lua snippet. A snippet for front matter

--- nvim/luasnippets/markdown.lua
+++ nvim/luasnippets/markdown.lua
@@ -1,15 +1,15 @@
 local ls = require("luasnip")
 -- some shorthands...
 local s = ls.snippet
-local n = ls.snippet_node
-local t = ls.text_node
+--local n = ls.snippet_node
+--local t = ls.text_node
 local i = ls.insert_node
 local f = ls.function_node
-local c = ls.choice_node
-local d = ls.dynamic_node
+--local c = ls.choice_node
+--local d = ls.dynamic_node
 
-local extras = require("luasnip.extras")
-local rep = extras.rep
+--local extras = require("luasnip.extras")
+--local rep = extras.rep
 local fmt = require("luasnip.extras.fmt").fmt
 
 -- Keymap for insert and selections of snippets
@@ -33,6 +33,10 @@ end, { silent = true })
 -- Markdown snippets =======================================
 local date = function()
   return { os.date("%B %d,%Y") }
+end
+
+local date_iso = function()
+  return { os.date("%Y-%m-%d") }
 end
 
 ls.add_snippets("markdown", {
@@ -59,6 +63,31 @@ ls.add_snippets("markdown", {
         i(1, "Code-lang"),
         i(2, "Past Code here!"),
         i(0),
+      }
+    )
+  ),
+
+  s(
+    {
+      trig = "fm",
+      dscr = "front matter markdown",
+    },
+    fmt(
+      [=[
+        ---
+        title: {}
+        author: {}
+        date: {}
+        description: {}
+        layout: {}
+        ---
+      ]=],
+      {
+        i(1, '"My blog post title"'),
+        i(2, '"Jaime Ledesma"'),
+        f(date_iso, {}),
+        i(3, '"Description of this post"'),
+        i(4, '"../../layouts/bloglayout.astro"'),
       }
     )
   ),
